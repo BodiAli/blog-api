bcbc360f1746cf302c2eeebbddccd9e16fe366b0
Bodi Ali <bodiali59@gmail.com>
2025-02-26 14:00:25 +0200

    Configure jwt and local strategies to use in passport.js

---
+++ config/passportConfig.js
@@ -0,0 +1,61 @@
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const { Strategy: LocalStrategy } = require("passport-local");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const prismaClient = require("../prisma/prismaClient");
+
+passport.use(
+  new LocalStrategy({ usernameField: "email", session: false }, async (email, password, done) => {
+    try {
+      const user = await prismaClient.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        done(null, false, { message: "Incorrect email or password" });
+        return;
+      }
+
+      const matchPassword = await bcrypt.compare(password, user.password);
+
+      if (!matchPassword) {
+        done(null, false, { message: "Incorrect email or password" });
+        return;
+      }
+
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  })
+);
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET_KEY,
+    },
+    async (payload, done) => {
+      try {
+        console.log(payload);
+        // TODO: Check the info argument in the done callback
+        const user = await prismaClient.user.findUnique({
+          where: {
+            id: payload.sub,
+          },
+        });
+
+        if (!user) {
+          done(null, false);
+          return;
+        }
+        done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
