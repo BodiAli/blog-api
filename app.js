6292f76609fafc231d80ac0a7d67fcc192a67886
Bodi Ali <bodiali59@gmail.com>
2025-02-26 17:56:48 +0200

    Import usersRouter for /users route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const postsRouter = require("./routes/postsRouter");
+const usersRouter = require("./routes/usersRouter");
 
 require("./config/passportConfig");
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 
 const port = process.env.PORT || 3000;
