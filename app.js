f3471d65518ba18ac34b154312471f062a917086
Bodi Ali <bodiali59@gmail.com>
2025-02-26 16:52:06 +0200

    Initialize posts route && handle parsing body

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const postsRouter = require("./routes/postsRouter");
 
 require("./config/passportConfig");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/posts", postsRouter);
 
 const port = process.env.PORT || 3000;
 
