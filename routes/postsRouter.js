e9fc46f4e85e916ef3aafd69c4934c55aa43c728
Bodi Ali <bodiali59@gmail.com>
2025-03-02 17:43:29 +0200

    Handle routes to get a single post and creating a post

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -4,5 +4,8 @@ const postsController = require("../controllers/postsController");
 const postsRouter = Router();
 
 postsRouter.get("/", postsController.getPosts);
+postsRouter.get("/:postId", postsController.getPost);
+
+postsRouter.post("/", postsController.createPost);
 
 module.exports = postsRouter;
