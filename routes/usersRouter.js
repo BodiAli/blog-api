06298674b974333d5d4bb48ad106e6b5b9508649
Bodi Ali <bodiali59@gmail.com>
2025-04-04 11:20:34 +0200

    Handle get requests to get user

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,6 +3,8 @@ const usersController = require("../controllers/usersController");
 
 const usersRouter = Router();
 
+usersRouter.get("/:userId", usersController.getUser);
+
 usersRouter.post("/sign-up", usersController.createUser);
 usersRouter.post("/log-in", usersController.authenticateUser);
 
