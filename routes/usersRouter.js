99b8665ca8254190d68796662edf04cca45a0f71
Bodi Ali <bodiali59@gmail.com>
2025-04-15 19:44:09 +0200

    Fix: Move handling signing up users and logging users requests to a differnt router && get posts instead of user

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,9 +3,6 @@ const usersController = require("../controllers/usersController");
 
 const usersRouter = Router();
 
-usersRouter.get("/:userId", usersController.getUser);
-
-usersRouter.post("/sign-up", usersController.createUser);
-usersRouter.post("/log-in", usersController.authenticateUser);
+usersRouter.get("/posts", usersController.getUserPosts);
 
 module.exports = usersRouter;
