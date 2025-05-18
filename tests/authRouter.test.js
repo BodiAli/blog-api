78235ffceed64e474cd5fb6c5339c7f3b6a6875b
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-05-18 19:51:14 +0300

    Feat: Create tests for sign up, login, and validate controllers and routes

--- tests/authRouter.test.js
+++ tests/authRouter.test.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const cloudinary = require("../config/cloudinaryConfig");
 const authRouter = require("../routes/authRouter");
 const prisma = require("../prisma/prismaClient");
+const issueJwt = require("../lib/issueJWT");
+
+require("../config/passportConfig");
 
 const app = express();
 
@@ -9,27 +14,143 @@ app.use(express.json());
 
 app.use("/auth", authRouter);
 
-jest.mock("jsonwebtoken", () => ({
-  sign: () => "token",
-}));
+jest.spyOn(jwt, "sign");
 
-beforeEach(async () => {
-  await prisma.user.deleteMany();
-});
+jest
+  .spyOn(cloudinary.uploader, "upload")
+  .mockImplementation(() => Promise.resolve({ secure_url: "url", public_id: "id" }));
 
 describe("authRouter Routes", () => {
-  test("signup auth route works", (done) => {
-    const buffer = Buffer.alloc(10);
+  afterAll(async () => {
+    await prisma.user.deleteMany();
+  });
 
+  beforeEach(() => {
+    jwt.sign.mockImplementation(() => "token");
+  });
+  const buffer = Buffer.alloc(10);
+
+  test("signup auth route works", (done) => {
     request(app)
       .post("/auth/sign-up")
-      .attach("userImage", buffer, {})
+      .attach("userImage", buffer, { filename: "profilePicture.jpg" })
       .field("firstName", "bodi")
       .field("lastName", "ali")
       .field("email", "bodi@gmail.com")
       .field("password", "12345")
       .field("confirmPassword", "12345")
+      .expect("Content-Type", /json/)
       .expect({ token: "Bearer token" })
-      .expect(201, done);
+      .expect(201)
+      .then(async () => {
+        expect(await prisma.user.count()).toEqual(1);
+        done();
+      });
+  });
+
+  test("signup route should send a 400 bad request with errors in the response body if input request body is invalid", (done) => {
+    request(app)
+      .post("/auth/sign-up")
+      .attach("userImage", buffer, { filename: "invalidType.jpg", contentType: "video/mp4" })
+      .field("firstName", "bodi")
+      .expect({
+        errors: [
+          {
+            type: "field",
+            value: "",
+            msg: "Last Name can not be empty.",
+            path: "lastName",
+            location: "body",
+          },
+          {
+            type: "field",
+            value: "",
+            msg: "Email can not be empty.",
+            path: "email",
+            location: "body",
+          },
+          {
+            type: "field",
+            value: "",
+            msg: "Email must be a valid email.",
+            path: "email",
+            location: "body",
+          },
+          {
+            type: "field",
+            value: "",
+            msg: "Password can not be empty.",
+            path: "password",
+            location: "body",
+          },
+          {
+            type: "field",
+            value: "",
+            msg: "Password must be at least 5 characters.",
+            path: "password",
+            location: "body",
+          },
+          {
+            type: "field",
+            msg: "Password and password confirmation do not match.",
+            path: "confirmPassword",
+            location: "body",
+          },
+          {
+            type: "field",
+            msg: "File uploaded is not of type image.",
+            path: "userImage",
+            location: "body",
+          },
+        ],
+      })
+      .expect("Content-Type", /json/)
+      .expect(400, done);
+  });
+
+  test("login auth route works", (done) => {
+    request(app)
+      .post("/auth/log-in")
+      .type("json")
+      .send({ email: "bodi@gmail.com", password: "12345" })
+      .expect("Content-Type", /json/, done)
+      .expect({ token: "Bearer token" })
+      .expect(200);
+  });
+
+  test("login controller should send a 400 status code if email or password are invalid", (done) => {
+    request(app)
+      .post("/auth/log-in")
+      .type("json")
+      .send({ email: "bodi@gmail.com", password: "invalid password" })
+      .expect("Content-Type", /json/)
+      .expect({ error: "Incorrect email or password" }, done)
+      .expect(401);
+  });
+
+  test("validate route should check if token is valid or not and send user object if valid", async () => {
+    jwt.sign.mockRestore();
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: "bodi@gmail.com",
+      },
+      include: {
+        Profile: {
+          select: {
+            profileImgUrl: true,
+          },
+        },
+      },
+      omit: {
+        email: true,
+        password: true,
+        profileId: true,
+      },
+    });
+
+    const token = issueJwt(user);
+
+    await request(app).get("/auth/validate").auth(token, { type: "bearer" }).expect(user).expect(200);
   });
 });
